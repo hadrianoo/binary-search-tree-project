fb90f12fa8201c6f8d70f58f5259393814055190
hadriano <57418210+hadrianoo@users.noreply.github.com>
2026-06-11 13:09:16 +0200

    fix: remove unneded return

--- src/index.js
+++ src/index.js
@@ -121,7 +121,7 @@ class Tree {
     if (firstElement.right !== null) {
       queue.push(firstElement.right);
     }
-    return this.levelOrderForEachRecursive(callback, queue);
+    this.levelOrderForEachRecursive(callback, queue);
   }
 }
 // const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
